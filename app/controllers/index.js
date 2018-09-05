dd5676c6aae284e34473471cd14e026ea1b5726f
pedatela <lucaspedatela@gmail.com>
2018-09-05 20:57:11 -0300

    Criando o controller de autenticação

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,18 @@
 module.exports.index = function(application, req, res) {
-  res.render('index');
+  res.render('index', {validacao: {}});
+};
+
+module.exports.autenticar = function(application, req, res) {
+  let dadosForm = req.body;
+
+  req.assert('usuario', 'Usuário não pode ser vazio').notEmpty();
+  req.assert('senha', 'Senha não pode ser vazia').notEmpty();
+
+  let erros = req.validationErrors();
+
+  if (erros) {
+    res.render("index", {validacao:erros});
+    return
+  }
+  res.send('ta tudo ok para a sessão');
 };

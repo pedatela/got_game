1d4c29a2ff3b08fe155b28fc516eee4a049f5d52
pedatela <lucaspedatela@gmail.com>
2018-09-05 22:10:19 -0300

    Ajustando a bandeira da casa do jogador

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,6 +1,6 @@
 module.exports.jogo = function(application, req, res) {
   if (req.session.autorizado) {
-    res.render('jogo');
+    res.render('jogo', {img_casa: req.session.casa});
   }else {
     res.send('Usuário precisa fazer o login');
   }

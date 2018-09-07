65e7b756360e39b4b670cd19537a56ada07e1698
pedatela <lucaspedatela@gmail.com>
2018-09-07 14:53:31 -0300

    Ações com súditos-recebendo e validando dados

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -4,12 +4,20 @@ module.exports.jogo = function(application, req, res) {
     return
   }
 
+  let comando_invalido = 'N';
+  if (req.query.comando_invalido == 'S') {
+    comando_invalido = 'S'
+  }
+
+
+
   let usuario = req.session.usuario;
   let casa = req.session.casa
+
   let connection = application.config.dbConnection;
   let JogoDAO = new application.app.models.JogoDAO(connection);
 
-  JogoDAO.iniciaJogo(res, usuario, casa);
+  JogoDAO.iniciaJogo(res, usuario, casa, comando_invalido);
 };
 
 
@@ -20,9 +28,37 @@ module.exports.sair = function(application, req, res) {
 };
 
 module.exports.suditos = function(application, req, res) {
+  if (req.session.autorizado !== true) {
+    res.send('Usuário precisa fazer o login');
+    return
+  }
   res.render('aldeoes', {validacao:{}});
 };
 
 module.exports.pergaminhos = function(application, req, res) {
+  if (req.session.autorizado !== true) {
+    res.send('Usuário precisa fazer o login');
+    return
+  }
   res.render('pergaminhos', {validacao:{}});
+};
+
+module.exports.ordenar_acao_sudito = function(application, req, res) {
+  if (req.session.autorizado !== true) {
+    res.send('Usuário precisa fazer o login');
+    return
+  }
+  
+  let dadosForm = req.body;
+
+  req.assert('acao', 'Ação deve ser informada').notEmpty();
+  req.assert('quantidade', 'Quantidade deve ser informada').notEmpty();
+
+  let erros = req.validationErrors();
+
+  if (erros){
+    res.redirect('jogo?comando_invalido=S');
+    return
+  }
+  res.send('tudo ok!')
 };
